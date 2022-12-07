072f7c9c7c77b3e60c0ff8e66770ddc1f61b99b1
ShaneB8188 <bonzani88@gmail.com>
2022-12-07 14:21:05 -0500

    fixed merge error

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,11 @@
+
+
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'home' }">Employee Login</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      <router-view></router-view>
+      <router-link v-bind:to="{ name: 'menu' }">Menu</router-link>&nbsp;|&nbsp;
     </div>
   </div>
 </template>
