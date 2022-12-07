c7ea6e51b3a982dbe84098a8425457ed7bbc2684
ShaneB8188 <bonzani88@gmail.com>
2022-12-07 14:45:03 -0500

    tryna merge?

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,7 +5,7 @@
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Employee Login</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      <router-link v-bind:to="{ name: 'menu' }">Menu</router-link>&nbsp;|&nbsp;
+
     </div>
   </div>
 </template>
