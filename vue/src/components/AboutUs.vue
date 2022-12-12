d81a2e26cdb5e54017bb13424b6dcd84261855f2
ShaneB8188 <bonzani88@gmail.com>
2022-12-12 13:08:28 -0500

    About us present

--- vue/src/components/AboutUs.vue
+++ vue/src/components/AboutUs.vue
@@ -1,66 +1,80 @@
 <template>
   <div>
-      <div class="card">
+    <div class="card">
       <header class="card-header">
         <p class="card-header-title">About Us</p>
       </header>
-      
+
+      <div class="card-content">‘Organization Name’</div>
+
       <div class="card-content">
-          ‘Organization Name’
+        The key to progress is much similar to the key to improving a pizza –
+        the more you put into it, the more you receive in return. Our pizza
+        family is as eager for flawlessness today as we were the point at which
+        we originally opened our entryways over 40 years back. Also, we’re
+        headed to be the best at making creative new items and formulas. Quality
+        is at our center. It’s the establishment we begun with, from the main
+        ‘Organization Name’ pizza that was made in a sweeper storage room in
+        Manhattan, IN, to now in excess of 5,000 areas in 45 nations and regions
+        around the globe. We don’t utilize shabby and increasingly prepared
+        fixings. Regardless of whether it’s our mark sauce, fixings, our unique
+        new mixture, or even the case itself, we put resources into our fixings
+        to guarantee that we generally give you the best quality pizza. For you,
+        it’s not only a superior pizza. It’s a family assembling, important
+        birthday, work festivity or basically an incredible supper. It’s our
+        objective to ensure you generally have the best elements for each event.
       </div>
 
-      <div class="card-content">
-          The key to progress is much similar to the key to improving a pizza – the more you put into it, the more you receive in return. Our pizza family is as eager for flawlessness today as we were the point at which we originally opened our entryways over 40 years back. Also, we’re headed to be the best at making creative new items and formulas.
+      <div class="card-content" id="value">Core interest</div>
 
-            Quality is at our center. It’s the establishment we begun with, from the main ‘Organization Name’ pizza that was made in a sweeper storage room in Manhattan, IN, to now in excess of 5,000 areas in 45 nations and regions around the globe.
+      <div class="card-content">
+        We should keep priority the main thing , The Main Thing which is pizza.
+        We will reliably convey a conventional ‘Organization Name’ unrivaled
+        quality pizza.
+      </div>
+
+      <div class="card-content" id="value">Predominance</div>
+
+      <div class="card-content">
+        Our consumer loyalty must be predictable, quantifiable and verifiable.
+        At ‘Organization Name’ we anticipate perfection – the “best in its
+        class” in all that we do.
+      </div>
+
+      <div class="card-content" id="value">Thought process</div>
+
+      <div class="card-content">
+        Individuals Are Priority Always. Our prosperity relies on our capacity,
+        as a group, to cooperate to accomplish our objectives and desires.
+      </div>
+
+      <div class="card-content" id="value">Disposition</div>
+
+      <div class="card-content">
+        On the off chance that you want to or you figure you can’t – you’re
+        correct! The distinction among champs and failures is a positive mental
+        frame of mind. Our frame of mind is an impression of what we esteem:
+        fruitful colleagues must be perky, proactive and enthusiastic about all
+        that they do.
+      </div>
+
+      <div class="card-content" id="value">Steady Improvement</div>
+
+      <div class="card-content">
+        We try constantly to outperform our past best. We continually “Increase
+        present expectations.” No issue how great we will be, we will dependably
+        show signs of improvement.
+      </div>
     </div>
-
-
-
-We don’t utilize shabby and increasingly prepared fixings. Regardless of whether it’s our mark sauce, fixings, our unique new mixture, or even the case itself, we put resources into our fixings to guarantee that we generally give you the best quality pizza.
-
-For you, it’s not only a superior pizza. It’s a family assembling, important birthday, work festivity or basically an incredible supper. It’s our objective to ensure you generally have the best elements for each event.
-
-Core interest
-
-We should keep priority the main thing , The Main Thing which is pizza. We will reliably convey a conventional ‘Organization Name’ unrivaled quality pizza.
-
-Responsibility
-
-We do what we state we will do when we state we will do it. We win the privilege to hold others to a larger amount of responsibility by being responsible to ourselves, our clients and our colleagues.
-
-Predominance
-
-Our consumer loyalty must be predictable, quantifiable and verifiable. At ‘Organization Name’ we anticipate perfection – the “best in its class” in all that we do.
-
-Thought process
-
-Individuals Are Priority Always. Our prosperity relies on our capacity, as a group, to cooperate to accomplish our objectives and desires.
-
-Disposition
-
-On the off chance that you want to or you figure you can’t – you’re correct! The distinction among champs and failures is a positive mental frame of mind. Our frame of mind is an impression of what we esteem: fruitful colleagues must be perky, proactive and enthusiastic about all that they do.
-
-Steady Improvement
-
-We try constantly to outperform our past best. We continually “Increase present expectations.” No issue how great we will be, we will dependably show signs of improvement.
-
-
-
-     </div>
-      </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-.card-content{
-    justify-content: center;
+.card-content {
+  justify-content: center;
 }
-
 </style>
