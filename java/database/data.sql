7ebf9e046f7099f903dc695f62196b8e109019db
Rohun Joseph <rox256@gmail.com>
2022-12-14 10:23:33 -0500

    Updated Pizza pictures

--- java/database/data.sql
+++ java/database/data.sql
@@ -10,12 +10,12 @@ INSERT into topping (name,price,isPremium,isAvailable) VALUES ('Sausage',1,false
 INSERT into topping (name,price,isPremium,isAvailable) VALUES ('Ham',1,false,true);
 INSERT into topping (name,price,isPremium,isAvailable) VALUES ('Banana Peppers',1,false,true);
 
-INSERT into specialty_pizzas (name, pizza_size, crust, sauce, image_url) VALUES ('Pepperoni', 'Large', 'Regular', 'Red', 'Assests/Pepperoni.jpeg');
-INSERT into specialty_pizzas (name, pizza_size, crust, sauce, image_url) VALUES ('Supreme', 'Large', 'Regular', 'Red', 'Assests/supreme.jfif');
-INSERT into specialty_pizzas (name, pizza_size, crust, sauce,image_url) VALUES ('Meat Lovers', 'Large', 'Regular', 'Red', 'Assests/meaty.jfif');
+INSERT into specialty_pizzas (name, pizza_size, crust, sauce, image_url) VALUES ('Pepperoni', 'Large', 'Regular', 'Red', 'Assests/pepperoni-za.jpg');
+INSERT into specialty_pizzas (name, pizza_size, crust, sauce, image_url) VALUES ('Supreme', 'Large', 'Regular', 'Red', 'Assests/supreme-za.jpg');
+INSERT into specialty_pizzas (name, pizza_size, crust, sauce,image_url) VALUES ('Meat Lovers', 'Large', 'Regular', 'Red', 'Assests/meat-lovers.jpg');
 INSERT into specialty_pizzas (name, pizza_size, crust, sauce,image_url) VALUES ('Veggie', 'Large', 'Regular', 'Red','Assests/Vegetarian-Pizza-720x720.jpg');
-INSERT into specialty_pizzas (name, pizza_size, crust, sauce,image_url) VALUES ('Neopolitan', 'Large', 'Regular', 'Red','Assests/neo2.jfif');
-INSERT into specialty_pizzas (name, pizza_size, crust, sauce,image_url) VALUES ('Cheese', 'Large', 'Regular', 'Red', 'Assests/cheese.jfif');
+INSERT into specialty_pizzas (name, pizza_size, crust, sauce,image_url) VALUES ('Neopolitan', 'Large', 'Regular', 'Red','Assests/neapolitan-za.jpg');
+INSERT into specialty_pizzas (name, pizza_size, crust, sauce,image_url) VALUES ('Cheese', 'Large', 'Regular', 'Red', 'Assests/cheese-za.jpg');
 
 INSERT into specialty_pizza_toppings (pizza_id, topping_id) VALUES (1, 2);
 INSERT into specialty_pizza_toppings (pizza_id, topping_id) VALUES (2, 4);
