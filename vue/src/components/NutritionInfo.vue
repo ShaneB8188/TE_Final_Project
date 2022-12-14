d611db4ca9b9cdefeddb32d84b5296b6a54f8787
Henry Isaac <hti1@pitt.edu>
2022-12-14 15:41:47 -0500

    nutrition formatting

--- vue/src/components/NutritionInfo.vue
+++ vue/src/components/NutritionInfo.vue
@@ -4,7 +4,49 @@
       <h2>Topping: {{ topping.name }}</h2>
     </div>
     <div>
-      {{ nutrition["foods"][0] }}
+      Amount(grams):
+      {{ nutritionPull.serving_weight_grams }}
+    </div>
+
+    <div>
+      Calories:
+      {{ nutritionPull.nf_calories }}
+    </div>
+    <div>
+      Total Fat(grams):
+      {{ nutritionPull.nf_total_fat }}
+    </div>
+    <div>
+      Saturated Fat(grams):
+      {{ nutritionPull.nf_saturated_fat }}
+    </div>
+    <div>
+      Cholesterol(milligrams):
+      {{ nutritionPull.nf_cholesterol }}
+    </div>
+    <div>
+      Sodium(milligrams):
+      {{ nutritionPull.nf_sodium }}
+    </div>
+    <div>
+      Total Carbs(grams):
+      {{ nutritionPull.nf_total_carbohydrate }}
+    </div>
+    <div>
+      Dietary Fiber(grams):
+      {{ nutritionPull.nf_dietary_fiber }}
+    </div>
+    <div>
+      Sugars(grams):
+      {{ nutritionPull.nf_sugars }}
+    </div>
+    <div>
+      Protein(grams):
+      {{ nutritionPull.nf_protein }}
+    </div>
+    <div>
+      Potassium(milligrams):
+      {{ nutritionPull.nf_potassium }}
     </div>
   </div>
 </template>
@@ -24,24 +66,15 @@ export default {
     };
   },
   computed: {
-    //pulls information out of returned nested array of size 1
-    // nutritionPull() {
-    //   return this.nutrition["foods"][0];
-    //}
+    // pulls information out of returned nested array of size 1
+    // <div>
+    //   {{ nutrition["foods"][0] }}
+    // </div>
+    nutritionPull() {
+      return this.nutrition["foods"][0];
+    },
   },
   props: ["topping"],
-  // this.name = name;
-  // this.grams = grams;
-  // this.calories = calories;
-  // this.totalFat = totalFat;
-  // this.saturatedFat = saturatedFat;
-  // this.cholesterol = cholesterol;
-  // this.sodium = sodium;
-  // this.totalCarbs = totalCarbs;
-  // this.dietaryFiber = dietaryFiber;
-  // this.sugars = sugars;
-  // this.protein = protein;
-  // this.potassium = potassium;
 };
 </script>
 
